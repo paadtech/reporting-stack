0f5ee2295f1773191f916e4b5b140646fb3fbcb3
p-stav <paulstavropoulos@gmail.com>
2017-01-27 00:13:31 -0800

    first commit
    Beginnings of parse dayshift

---
+++ sandbox/parse_dailyshet_to_statistics.py
@@ -0,0 +1,36 @@
+"""
+The purpose of this python document is to parse through the daily sheet uploads
+and format the data into the necessary format to be uploaded to a sql server
+
+
+Sheet 1:
+Sheet 2: Provider Summary
+Sheet 3: Deposit Slip
+Sheet 4: Credit Card Slips to deposit
+"""
+
+#import necessary read excel docs
+import xlrd
+import csv # not sure if I'll need this
+
+
+
+######Work for sheet 1
+
+
+
+
+
+
+######Work for sheet 2
+
+
+
+
+
+######Work for sheet 3
+
+
+
+
+######Work for sheet 4
