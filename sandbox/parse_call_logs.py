5d9196c7e3253a2beca2b8f31ad531b55681cc15
p-stav <paulstavropoulos@gmail.com>
2017-02-02 22:42:06 -0800

    finished parser for call logs

---
+++ sandbox/parse_call_logs.py
@@ -0,0 +1,167 @@
+"""
+### Kouvaya ###
+The purpose of this python document is to parse through the call logs created by the PAAD office
+and format the data into so as to enter row in the master file where 1 row will represent 1 day's worth of activity.
+This architecture will allow for much easier viewing and editing on the part of admins
+of the PAAD.tech@gmail.com account
+
+"""
+
+import xlrd
+import csv
+from datetime import datetime
+import os # used to access files
+import glob # used to grab items
+
+#### define needed function to initlize dictionary for every unique date found
+def initializeDict():
+    temp = {}
+    temp["new_patients"] =0
+    temp["number_answered_phone"] = 0
+    temp["number_left_messages"] = 0
+    temp["number_recall_emails"] = 0
+    temp["appts_scheduled"] = 0
+    temp["number_phones_disconnected"] = 0
+    temp["number_people_moved"] = 0
+    temp["number_local_office_changes"] = 0
+    temp["reports_by"] = {}
+
+    return temp
+
+
+# open csv in append mode
+skeleton = open("master_call_log_file.csv", "a")
+writer = csv.writer(skeleton, delimiter=",")
+
+#create headline
+### note: on the cloud, this skeleton will have already been created
+#writer.writerow(["date", "New Patients", "Number Answered Phone", "Number of Left Messages", "Number of Recall Emails Sent", "Appts Scheduled", "Number Phones Disconnected", "Number of People Moved", "Number of Local Office Changes", "Reports By"])
+
+
+# Create arrays of Yes and no answers
+yes_checks = ["y", "yes"]
+#no_checks = ["n", "no", "na"]
+
+
+# create path to file name
+# TODO: this will change on the server
+# TODO: number of files we grab will also change
+os.chdir("/Users/paulstavropoulos/Google Drive/kouvaya/reporting-stack/example_docs")
+call_logs = glob.glob("*.csv")
+
+
+#loop through all items
+for item in call_logs:
+    try:
+        # open item
+        open_item = open(item, "rb")
+        log = csv.reader(open_item)
+
+
+        # create a dictionary that will store dictionaries for every unique date found
+        dates = {}
+
+        passed_header = False
+        for row in log:
+            #create boolean to check if we passed row where first item is "date"
+            # we want to start parsing rows only when we've passed the header row
+            # which we can't bet consistently occurs in the same spot
+            try:
+                if 'date' in row[0].lower():
+                    passed_header = True
+                    continue
+            except:
+                pass
+
+            # once we've passed header, then start
+            if passed_header == True:
+
+                # check to see if first item has a date that is dictionary.
+                # if not, initialize it and add
+                try:
+                    if row[0] not in dates.keys():
+                        dictionary = initializeDict()
+                        dates[row[0]] = dictionary
+                except:
+                    pass
+
+                # for every column, perform a series of yes checks and appts_scheduled
+                #new patient
+                try:
+                    if row[2].lower() in yes_checks:
+                        dates[row[0]]["new_patients"] += 1
+                except:
+                    pass
+
+                try:
+                    #did answer
+                    if row[3].lower() in yes_checks:
+                        dates[row[0]]["number_answered_phone"] += 1
+                except:
+                    pass
+
+                try:
+                    #did leave Messages
+                    if row[4].lower() in yes_checks:
+                        dates[row[0]]["number_left_messages"] += 1
+                except:
+                    pass
+
+                try:
+                    #number recall emails
+                    if row[5].lower() in yes_checks:
+                        dates[row[0]]["number_recall_emails"] += 1
+                except:
+                    pass
+
+                try:
+                    # appts Scheduled
+                    if row[6].lower() in yes_checks:
+                        dates[row[0]]["appts_scheduled"] += 1
+                except:
+                    pass
+
+                try:
+                    #phone Disconnected
+                    if row[7].lower() in yes_checks:
+                        dates[row[0]]["number_phones_disconnected"] += 1
+                except:
+                    pass
+
+                try:
+                    # person moved
+                    if row[8].lower() in yes_checks:
+                        dates[row[0]]["number_people_moved"] += 1
+                except:
+                    pass
+
+                try:
+                    # changed local office
+                    if row[9].lower() in yes_checks:
+                        dates[row[0]]["number_local_office_changes"] += 1
+                except:
+                    pass
+
+                try:
+                    # add rows for every reporter
+                    if row[10].lower().strip() in dates[row[0]]["reports_by"].keys():
+                        dates[row[0]]["reports_by"][row[10].lower().strip()] += 1
+                    if row[10].lower().strip() not in dates[row[0]]["reports_by"].keys():
+                        dates[row[0]]["reports_by"][row[10].lower().strip()] = 1
+                except:
+                    pass
+
+        for key in dates.keys():
+            try:
+                writer.writerow([key, dates[key]["new_patients"], dates[key]["number_answered_phone"], dates[key]["number_left_messages"], dates[key]["number_recall_emails"], dates[key]["appts_scheduled"], dates[key]["number_phones_disconnected"], dates[key]["number_people_moved"], dates[key]["number_local_office_changes"], dates[key]["reports_by"]])
+            except:
+                pass
+
+        # close log we opened
+        open_item.close()
+
+    except:
+        pass
+
+#close writer
+skeleton.close()
--- sandbox/parse_dailysheet_skeleton.py
+++ sandbox/parse_dailysheet_skeleton.py
@@ -22,120 +22,141 @@ import os # used to access files
 import glob # used to grab items
 
 
-# create CSV
-skeleton = open("skeleton_master_file.csv", "wb")
+# open csv in append mode
+skeleton = open("master_file.csv", "a")
 writer = csv.writer(skeleton, delimiter=",")
 
 #create headline
 ### note: on the cloud, this skeleton will have already been created
-writer.writerow(["Date", "Patients", "New Patients", "Production", "Production per Provider", "Cash Collections", "Check Collections", "Credit Card Collections", "Collections Per Provider", "Ortho Cases", "Hygiene Cases" ])
+# writer.writerow(["Date", "Patients", "New Patients", "Production", "Production per Provider", "Cash Collections", "Check Collections", "Credit Card Collections", "Collections Per Provider", "Ortho Cases", "Hygiene Cases" ])
 
 ## get a list of files and loop through them!
+# TODO: change location on the cloud
 os.chdir("/Users/paulstavropoulos/Google Drive/kouvaya/reporting-stack/jan_daily_sheets")
 daysheets = glob.glob("*.xls*")
 
 for item in daysheets:
     # open a dailysheet workbook to read from
-    #this here is a test document -->
     # on the VM server, we will use the Google API to grab the last dailysheets that have not been uploaded
     # perform this code on each of them
-    wkbk = xlrd.open_workbook(item)
+    try:
+        wkbk = xlrd.open_workbook(item)
 
-    '''
-    Work for sheet 1
-    Format for sheet 1:
-    rows 0-3 are headers, column 0 is trash
-    '''
-    sheet1 = wkbk.sheet_by_index(0)
-    row = 6
+        '''
+        Work for sheet 1
+        Format for sheet 1:
+        rows 0-3 are headers, column 0 is trash
+        '''
+        sheet1 = wkbk.sheet_by_index(0)
+        row = 6
 
-    # define the dictionary to store all information to print
-    # initialize items
-    daily_info = {}
-    daily_info["prod"] = daily_info["cash_collections"] = daily_info["check_collections"] =  daily_info["credit_collections"] = 0
-    daily_info["ortho_cases"] = daily_info["hygiene_cases"] = 0
-    daily_info["patients"] = daily_info["new_patients"] = 0
-    daily_info["prod_per_provider"] = {}
-    daily_info["collections_per_provider"] = {}
+        # define the dictionary to store all information to print
+        # initialize items
+        daily_info = {}
+        daily_info["prod"] = daily_info["cash_collections"] = daily_info["check_collections"] =  daily_info["credit_collections"] = 0
+        daily_info["ortho_cases"] = daily_info["hygiene_cases"] = 0
+        daily_info["patients"] = daily_info["new_patients"] = 0
+        daily_info["prod_per_provider"] = {}
+        daily_info["collections_per_provider"] = {}
 
-    # TODO: find out how to record billable insurance
-    # TODO: find out how to record referrals
-
-
-    # while loop --> end when column 6 is empty (Description)
-    # do i care that all rows are not unique to single patient? I don't think so
-    # in SQL we can do a search for a user and be fine (no duplicates either)
-    # row 6 is never empty when there is data --> keep looping until it is empty
-    while(sheet1.cell(row,6).value != ''):
-
-        # production created
-        if(sheet1.cell(row,7).value != ''):
-            daily_info["prod"] += sheet1.cell(row,7).value
-
-        #production created per provider/dentist
-        # check if dr code in dictionary prod_per_provider
-        # if it is, ad prod value. If it is not, initiate with prod value
-            if sheet1.cell(row,3).value != '' and sheet1.cell(row,7).value != '':
-                if sheet1.cell(row,3) in daily_info["prod_per_provider"].keys():
-                    daily_info["prod_per_provider"][int(sheet1.cell(row,3).value)] += sheet1.cell(row,7).value
-                else:
-                    daily_info["prod_per_provider"][int(sheet1.cell(row,3).value)] = sheet1.cell(row,7).value
-
-        # ortho cases and hygiene deposit
-        # check description and code columns
-        if 'invisalign' in sheet1.cell(row,6).value.lower():
-            daily_info["ortho_cases"] += 1
-        if sheet1.cell(row,5).value != '' and (int(sheet1.cell(row,5).value) == 1110 or int(sheet1.cell(row,5).value) == 4341 or int(sheet1.cell(row,5).value) == 1206):
-            daily_info["hygiene_cases"] += 1
-
-        # collections
-        # cash, credit, check
-        # also perform collections per provider work in this sections as well
-        # use dr. code to find out
-        if(sheet1.cell(row,10).value != ''):
-            daily_info['cash_collections'] += int(sheet1.cell(row,10).value)
-
-            # check to see if provider is in dictionary of collections_per_provider
-            if sheet1.cell(row,3).value in daily_info["collections_per_provider"].keys():
-                daily_info["collections_per_provider"][int(sheet1.cell(row,3).value)] += sheet1.cell(row,10).value
-            else:
-                daily_info["collections_per_provider"][int(sheet1.cell(row,3).value)] = sheet1.cell(row,10).value
-
-        if(sheet1.cell(row,11).value != ''):
-            daily_info['check_collections'] += int(sheet1.cell(row,11).value)
-
-            # check to see if provider is in dictionary of collections_per_provider
-            if sheet1.cell(row,3).value in daily_info["collections_per_provider"].keys():
-                daily_info["collections_per_provider"][int(sheet1.cell(row,3).value)] += sheet1.cell(row,11).value
-            else:
-                daily_info["collections_per_provider"][int(sheet1.cell(row,3).value)] = sheet1.cell(row,11).value
-
-        if(sheet1.cell(row,12).value != ''):
-            daily_info['credit_collections'] += int(sheet1.cell(row,12).value)
-
-            # check to see if provider is in dictionary of collections_per_provider
-            if sheet1.cell(row,3).value in daily_info["collections_per_provider"].keys():
-                daily_info["collections_per_provider"][int(sheet1.cell(row,3).value)] += sheet1.cell(row,12).value
-            else:
-                daily_info["collections_per_provider"][int(sheet1.cell(row,3).value)] = sheet1.cell(row,12).value
-
-        # increment row!
-        row += 1
-
-    # once we've reached the end of the list, there are a static number of columns
-    # between current row number and the information we want for summary metrics reported
-
-    # record patients and new patients
-    # look for cell that equals "patients" and "new patients"
-    if sheet1.cell(row + 14, 6).value != '':
-        daily_info["patients"] = int(sheet1.cell(row + 14, 6).value)
-    if sheet1.cell(row + 15, 6).value != '':
-        daily_info["new_patients"] = int(sheet1.cell(row + 15, 6).value)
+        # TODO: find out how to record billable insurance
+        # TODO: find out how to record referrals
 
 
+        # while loop --> end when column 6 is empty (Description)
+        # do i care that all rows are not unique to single patient? I don't think so
+        # in SQL we can do a search for a user and be fine (no duplicates either)
+        # row 6 is never empty when there is data --> keep looping until it is empty
+        while(sheet1.cell(row,6).value != ''):
 
-    # write to row in skeletion
-    #"Date", "Patients", "New Patients", "Production", "Production per Provider", "Cash Collections", "Check Collections",
-    #"Credit Card Collections", "Collections Per Provider", "Ortho Cases", "Hygiene Cases"
-    writer.writerow([datetime(*xlrd.xldate_as_tuple(sheet1.cell(2,0).value, wkbk.datemode)).date(), daily_info["patients"], daily_info["new_patients"], daily_info["prod"], daily_info["prod_per_provider"], daily_info["cash_collections"], daily_info["check_collections"], daily_info["credit_collections"], daily_info["collections_per_provider"], daily_info["ortho_cases"], daily_info["hygiene_cases"]
-    ])
+            # production created
+            try:
+                if(sheet1.cell(row,7).value != ''):
+                    daily_info["prod"] += sheet1.cell(row,7).value
+            except:
+                pass
+            #production created per provider/dentist
+            # check if dr code in dictionary prod_per_provider
+            # if it is, ad prod value. If it is not, initiate with prod value
+                try:
+                    if sheet1.cell(row,3).value != '' and sheet1.cell(row,7).value != '':
+                        if sheet1.cell(row,3) in daily_info["prod_per_provider"].keys():
+                            daily_info["prod_per_provider"][int(sheet1.cell(row,3).value)] += sheet1.cell(row,7).value
+                        else:
+                            daily_info["prod_per_provider"][int(sheet1.cell(row,3).value)] = sheet1.cell(row,7).value
+                except:
+                    pass
+            # ortho cases and hygiene deposit
+            # check description and code columns
+            try:
+                if 'invisalign' in sheet1.cell(row,6).value.lower():
+                    daily_info["ortho_cases"] += 1
+                if sheet1.cell(row,5).value != '' and (int(sheet1.cell(row,5).value) == 1110 or int(sheet1.cell(row,5).value) == 4341 or int(sheet1.cell(row,5).value) == 1206):
+                    daily_info["hygiene_cases"] += 1
+            except:
+                pass
+            # collections
+            # cash, credit, check
+            # also perform collections per provider work in this sections as well
+            # use dr. code to find out
+            if(sheet1.cell(row,10).value != ''):
+                try:
+                    daily_info['cash_collections'] += int(sheet1.cell(row,10).value)
+
+                    # check to see if provider is in dictionary of collections_per_provider
+                    if sheet1.cell(row,3).value in daily_info["collections_per_provider"].keys():
+                        daily_info["collections_per_provider"][int(sheet1.cell(row,3).value)] += sheet1.cell(row,10).value
+                    else:
+                        daily_info["collections_per_provider"][int(sheet1.cell(row,3).value)] = sheet1.cell(row,10).value
+                except:
+                    pass
+            if(sheet1.cell(row,11).value != ''):
+                try:
+                    daily_info['check_collections'] += int(sheet1.cell(row,11).value)
+
+                    # check to see if provider is in dictionary of collections_per_provider
+                    if sheet1.cell(row,3).value in daily_info["collections_per_provider"].keys():
+                        daily_info["collections_per_provider"][int(sheet1.cell(row,3).value)] += sheet1.cell(row,11).value
+                    else:
+                        daily_info["collections_per_provider"][int(sheet1.cell(row,3).value)] = sheet1.cell(row,11).value
+                except:
+                    pass
+
+            if(sheet1.cell(row,12).value != ''):
+                try:
+                    daily_info['credit_collections'] += int(sheet1.cell(row,12).value)
+
+                    # check to see if provider is in dictionary of collections_per_provider
+                    if sheet1.cell(row,3).value in daily_info["collections_per_provider"].keys():
+                        daily_info["collections_per_provider"][int(sheet1.cell(row,3).value)] += sheet1.cell(row,12).value
+                    else:
+                        daily_info["collections_per_provider"][int(sheet1.cell(row,3).value)] = sheet1.cell(row,12).value
+                except:
+                    pass
+            # increment row!
+            row += 1
+
+
+        # once we've reached the end of the list, there are a static number of columns
+        # between current row number and the information we want for summary metrics reported
+
+        # record patients and new patients
+        # look for cell that equals "patients" and "new patients"
+        try:
+            if sheet1.cell(row + 14, 6).value != '':
+                daily_info["patients"] = int(sheet1.cell(row + 14, 6).value)
+            if sheet1.cell(row + 15, 6).value != '':
+                daily_info["new_patients"] = int(sheet1.cell(row + 15, 6).value)
+        except:
+            pass
+
+
+
+        # write to row in skeletion
+        #"Date", "Patients", "New Patients", "Production", "Production per Provider", "Cash Collections", "Check Collections",
+        #"Credit Card Collections", "Collections Per Provider", "Ortho Cases", "Hygiene Cases"
+        writer.writerow([datetime(*xlrd.xldate_as_tuple(sheet1.cell(2,0).value, wkbk.datemode)).date(), daily_info["patients"], daily_info["new_patients"], daily_info["prod"], daily_info["prod_per_provider"], daily_info["cash_collections"], daily_info["check_collections"], daily_info["credit_collections"], daily_info["collections_per_provider"], daily_info["ortho_cases"], daily_info["hygiene_cases"]
+        ])
+
+    except:
+        pass
